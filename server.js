320f19add52a94f92f907b418943ec728cc9f2d5
Clam119 <Christopher.lam119@gmail.com>
2022-07-06 22:37:28 +0100

    setup indexRouter for individual routing

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts') 
 
+const indexRouter = require('./routes/index.js')
+
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views') //EJS Templating files
 app.set('layout', 'layouts/layout') //Create layout file with all files in here so you don't have to duplicate header/footers
+
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use('/', indexRouter)
 
 app.listen(process.env.PORT || 3000) //Will listen to port in the environment that it's hosted on, default to 3000 for production.
